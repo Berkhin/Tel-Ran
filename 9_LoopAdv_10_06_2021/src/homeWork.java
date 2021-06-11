7aa61d7a5aff3f3aaf45dd9276f4299fb9d575d8
Berkhin <71590601+Berkhin@users.noreply.github.com>
2021-06-12 02:28:19 +0300

    Add files via upload

---
+++ 9_LoopAdv_10_06_2021/src/homeWork.java
@@ -0,0 +1,32 @@
+
+public class homeWork {
+
+	public static void main(String[] args) {
+		int num = 12345;
+		int odd = oddDigits(num);
+		int even = evenDigits(num);
+		System.out.println("Sum odd digits = " + odd);
+		System.out.println("Sum even digits = " + even);
+
+	}
+
+	public static int oddDigits(int num) {
+		int res = 0;
+		while (num > 0) {
+			res += num % 10;
+			num /= 100;
+		}
+		return res;
+	}
+
+	public static int evenDigits(int num) {
+		int res = 0;
+		while (num > 0) {
+			num /= 10;
+			res += num % 10;
+			num /= 10;
+
+		}
+		return res;
+	}
+}
